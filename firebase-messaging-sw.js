f5ca67bcf7a15b7bf7c62aaa38902c0e35e6b5de
menupriceshub <seowithakshay@gmail.com>
2026-06-28 02:29:30 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/12.14.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/12.14.0/firebase-messaging-compat.js');
-
+importScripts("https://www.gstatic.com/firebasejs/12.14.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.14.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyCod1_H1HEGw2wUg3lzkC1OCKNfzQ17eho",
@@ -13,21 +12,44 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-self.addEventListener('notificationclick', function(event) {
+// Background notification
+messaging.onBackgroundMessage((payload) => {
+  console.log("Background message:", payload);
+
+  const notificationTitle =
+    payload.notification?.title || "New Notification";
+
+  const notificationOptions = {
+    body: payload.notification?.body || "",
+    icon: "/icon-192.png",
+    badge: "/icon-192.png",
+    image: payload.notification?.image,
+    data: payload.data || {}
+  };
+
+  self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
+
+// Notification click
+self.addEventListener("notificationclick", (event) => {
   event.notification.close();
 
-  let url = '/';
-
-  if (
-    event.notification.data &&
-    event.notification.data.FCM_MSG &&
-    event.notification.data.FCM_MSG.data &&
-    event.notification.data.FCM_MSG.data.url
-  ) {
-    url = event.notification.data.FCM_MSG.data.url;
-  }
+  const url = event.notification.data?.link || "/";
 
   event.waitUntil(
-    clients.openWindow(url)
+    clients.matchAll({ type: "window", includeUncontrolled: true })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.url === url && "focus" in client) {
+            return client.focus();
+          }
+        }
+        if (clients.openWindow) {
+          return clients.openWindow(url);
+        }
+      })
   );
 });
