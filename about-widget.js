3954a93076b3ac4d26652c58e770559422d3f7b4
menupriceshub <seowithakshay@gmail.com>
2026-07-28 15:02:15 +0530

    Create about-widget.js

---
+++ about-widget.js
@@ -0,0 +1,13 @@
+fetch("/about.json")
+.then(response => response.json())
+.then(data => {
+
+document.getElementById("about-title").innerHTML = data.title;
+
+document.getElementById("about-content").innerHTML =
+`
+${data.description}<br>
+${data.description2}
+`;
+
+});
