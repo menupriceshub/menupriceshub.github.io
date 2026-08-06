6d2fcb6b827d6736d5e5d2c23d884e840774ed48
menupriceshub <seowithakshay@gmail.com>
2026-08-07 01:31:31 +0530

    Create latest-blog1.js

---
+++ js/latest-blog1.js
@@ -0,0 +1,91 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const container = document.getElementById("mph-latest-blog");
+
+if(!container) return;
+
+
+fetch("/data/blog-posts.json")
+.then(res => res.json())
+.then(data => {
+
+
+let html = `
+
+<div class="section-header">
+
+<h2 class="section-left">
+Latest Blog Post
+</h2>
+
+<a href="/blog.html" class="section-right">
+View All
+<i class="fa-solid fa-chevron-right icon"></i>
+</a>
+
+</div>
+
+
+<div class="mph-latest-main">
+
+
+<a href="${data.featured.url}" class="mph-latest-featured">
+
+<img src="${data.featured.image}" 
+alt="${data.featured.title}">
+
+<div class="mph-latest-overlay">
+
+<span>Featured</span>
+
+<h3>
+${data.featured.title}
+</h3>
+
+</div>
+
+</a>
+
+
+<div class="mph-latest-list">
+
+`;
+
+
+
+data.posts.forEach(post => {
+
+html += `
+
+<a href="${post.url}" class="mph-latest-item">
+
+<img src="${post.image}" alt="${post.title}">
+
+<h4>
+${post.title}
+</h4>
+
+</a>
+
+`;
+
+});
+
+
+html += `
+
+</div>
+
+</div>
+
+`;
+
+container.innerHTML = html;
+
+
+})
+
+.catch(err => console.log("Blog Load Error:",err));
+
+
+});
