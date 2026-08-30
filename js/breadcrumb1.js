a2553263a92f32f8fede0ee4a691f33123ccb235
menupriceshub <seowithakshay@gmail.com>
2026-08-30 16:08:35 +0530

    Update breadcrumb1.js

--- js/breadcrumb1.js
+++ js/breadcrumb1.js
@@ -1,35 +1,112 @@
-(function(){
+(function () {
 
-const container = document.getElementById("breadcrumb1");
+  // ==============================
+  // GET CURRENT PATH
+  // ==============================
 
-const path = window.location.pathname
-.replace(/^\/|\/$/g,"")
-.split("/")
-.filter(Boolean);
+  const path = window.location.pathname
+    .replace(/^\/|\/$/g, "")
+    .split("/")
+    .filter(Boolean);
 
-let html = '<a href="/">Home</a>';
+  // ==============================
+  // VISIBLE BREADCRUMB
+  // ==============================
 
-let currentPath = "";
+  const container = document.getElementById("breadcrumb1");
 
-path.forEach((part,index)=>{
+  let html = '<a href="/">Home</a>';
 
-currentPath += "/" + part;
+  let currentPath = "";
 
-let name = part
-.replace(".html","")
-.replace(/[-_]/g," ")
-.replace(/\b\w/g,c=>c.toUpperCase());
+  path.forEach((part, index) => {
 
-html += '<span class="separator">›</span>';
+    currentPath += "/" + part;
 
-if(index === path.length-1){
-html += '<span>'+name+'</span>';
-}else{
-html += '<a href="'+currentPath+'/">'+name+'</a>';
-}
+    const isLast = index === path.length - 1;
 
-});
+    const name = part
+      .replace(/\.html$/, "")
+      .replace(/[-_]/g, " ")
+      .replace(/\b\w/g, c => c.toUpperCase());
 
-container.innerHTML = html;
+    html += '<span class="separator">›</span>';
+
+    if (isLast) {
+
+      html += '<span>' + name + '</span>';
+
+    } else {
+
+      // Folder/category pages
+      html += '<a href="' + currentPath + '/">' + name + '</a>';
+
+    }
+
+  });
+
+  if (container) {
+    container.innerHTML = html;
+  }
+
+
+  // ==============================
+  // BREADCRUMB SEO SCHEMA
+  // ==============================
+
+  const itemList = [];
+
+  // Home
+  itemList.push({
+    "@type": "ListItem",
+    "position": 1,
+    "name": "Home",
+    "item": window.location.origin + "/"
+  });
+
+  let schemaUrl = window.location.origin;
+
+  path.forEach((part, index) => {
+
+    schemaUrl += "/" + part;
+
+    const name = part
+      .replace(/\.html$/, "")
+      .replace(/[-_]/g, " ")
+      .replace(/\b\w/g, c => c.toUpperCase());
+
+    // Current page URL
+    let itemUrl = schemaUrl;
+
+    if (index < path.length - 1) {
+      itemUrl += "/";
+    }
+
+    itemList.push({
+      "@type": "ListItem",
+      "position": index + 2,
+      "name": name,
+      "item": itemUrl
+    });
+
+  });
+
+
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": itemList
+  };
+
+
+  // Add JSON-LD to <head>
+  const script = document.createElement("script");
+
+  script.type = "application/ld+json";
+
+  script.textContent = JSON.stringify(schema);
+
+  document.head.appendChild(script);
+
 
 })();
