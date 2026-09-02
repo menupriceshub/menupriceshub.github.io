5c9971b2287fff420531316180e7ad9e0ecc9276
menupriceshub <seowithakshay@gmail.com>
2026-09-02 15:59:44 +0530

    Add ad-widget.js to load Google Ads script

---
+++ js/ad-widget.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.async = true;
+script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4850266028754096";
+script.crossOrigin = "anonymous";
+document.head.appendChild(script);
